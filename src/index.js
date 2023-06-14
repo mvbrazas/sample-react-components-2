0c32f92fdf78e37610a82c11225757c534bbcb0f
Jasper <mvbrazas@gmail.com>
2023-06-14 16:57:54 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export * from './components';
+
+//npm install rollup @rollup/plugin-node-resolve @rollup/plugin-commonjs @rollup/plugin-babel @babel/preset-env @babel/preset-react rollup-plugin-postcss rollup-plugin-peer-deps-external --save-dev
